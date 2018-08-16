e50d239d84ca322bdcb82a2fad4b31786730b3ae
luozhonghua <172257861@qq.com>
2018-08-16 15:35:07 +0800

    add

---
+++ cloud-commons-pay/src/main/java/com/st/fox/commons/pay/constants/BizConstant.java
@@ -0,0 +1,15 @@
+package com.st.fox.commons.pay.constants;
+
+/**
+ * 下单业务常量
+ * 
+ * @author luozhonghua
+ */
+public interface BizConstant {
+
+	/**
+	 * 下单 XD
+	 */
+	String	PAY_ORDER_PREFIX	= "XD";
+
+}
---
+++ cloud-commons-utils/src/main/java/com/st/fox/commons/utils/StringHelper.java
@@ -0,0 +1,16 @@
+package com.st.fox.commons.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 字符串工具类, 继承org.apache.commons.lang3.StringUtils类
+ *
+ * @author luozhonghua
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class StringHelper extends StringUtils {
+
+}
